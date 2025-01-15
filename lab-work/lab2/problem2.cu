e1cb05f098fa09979fcadd77bbf372ebf2ca35bd
Uday Bhardwaj <udaybh07@gmail.com>
2025-01-15 22:29:44 +0530

    adding problem 2 for lab 2

--- lab-work/lab2/problem2.cu
+++ lab-work/lab2/problem2.cu
@@ -1,4 +1,6 @@
+%%cuda
 #include<stdio.h>
+#include <sys/time.h>
 
 __global__ void computeZ ( int *X, int *Y, int *Z, int a, int N ) {
     int i =  threadIdx.x;
@@ -8,8 +10,10 @@ __global__ void computeZ ( int *X, int *Y, int *Z, int a, int N ) {
 }
 
 int main() {
-    int k = 32;
+    int k = 2048;
     int a = 4;
+
+    time_t t0, t1;
 
     int *sX = new int[k*k];
     int *sY = new int[k*k];
@@ -23,10 +27,17 @@ int main() {
         X[i] = 1;  Y[i] = 2; sX[i]= 1; sY[i]= 2;
     }
 
-    gettimeofday(&t0, 0);
-/* ... */
-gettimeofday(&t1, 0);
-long elapsed = (t1.tv_sec-t0.tv_sec)*1000000 + t1.tv_usec-t0.tv_usec;
+    clock_t start = clock();
+
+    for ( int i=0;i<k;i++ ) {
+        for ( int j=0; j< k;j++ ) {
+            sZ[i*k+j]= sX[i*k+j] + sY[i*k+j]; 
+        }
+    }
+
+    clock_t end = clock();
+    double time_seq = (end - start)/(double)CLOCKS_PER_SEC;
+
 
     // Allocate memory for arrays X, Y, Z on the device
     int *d_X, *d_Y, *d_Z;
@@ -38,21 +49,23 @@ long elapsed = (t1.tv_sec-t0.tv_sec)*1000000 + t1.tv_usec-t0.tv_usec;
     cudaMemcpy(d_X, X, k*k * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(d_Y, Y, k*k* sizeof(int), cudaMemcpyHostToDevice);
 
+    
+    int THREAD_BLOCK_NO   =  4096 ;
+    int THREADS_PER_BLOCK =  1024 ;
+
+    start = clock();
 
     // Launch the kernel
     computeZ<<<1,k*k >>>(d_X, d_Y, d_Z, a, k*k);
+
+    end = clock();
+    double time_par = (end - start)/(double)CLOCKS_PER_SEC;
     
     // Copy the result array Z back to host
     cudaMemcpy(Z, d_Z, k*k * sizeof(int), cudaMemcpyDeviceToHost);
 
-    //  Optionally, print the result
-    for (int i = 0; i < k; ++i) {
-	for ( int j=0; j<k;j++ ) {
-       	   printf("%d ", Z[i*k+j]);
-	}
-       printf("\n");
-    }
-
+    printf("Number of thread block: %d , Threads per Block: %d \n\n Sequential Execution time: %lf, Cuda Kernel Speed up time: %lf", THREAD_BLOCK_NO, THREADS_PER_BLOCK, time_seq, time_par);
+    
     // Free device memory
     cudaFree(d_X);
     cudaFree(d_Y);
